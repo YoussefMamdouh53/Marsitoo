dd1b0d86703cf12cb7bbf1714fff729293e6ba94
Youssef Mamdouh <51208752+YoussefMamdouh53@users.noreply.github.com>
2022-11-04 19:19:47 +0200

    Add files via upload

---
+++ ProjectServer/client.cpp
@@ -0,0 +1,9 @@
+
+#include "clientnetwork.hpp"
+
+int main(int argc, char * argv[]){
+     ClientNetwork client_network;
+     client_network.Connect("localHost", 2525);
+     client_network.Run();
+     return 0;
+}
