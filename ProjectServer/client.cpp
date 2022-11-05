d5c48cde8630908c3c88004d9412a5e804d298be
Youssef Mamdouh <51208752+YoussefMamdouh53@users.noreply.github.com>
2022-11-05 12:51:40 +0200

    Delete ProjectServer directory

--- ProjectServer/client.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "clientnetwork.hpp"
-
-int main(int argc, char * argv[]){
-     ClientNetwork client_network;
-     client_network.Connect("localHost", 2525);
-     client_network.Run();
-     return 0;
-}
